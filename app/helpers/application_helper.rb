57c98374419a51a39218d8e5853da65895dd0186
Ravi P <ravip0711@gmail.com>
2016-02-15 17:30:25 -0700

    fixed delete comments glyphicon, and edit post photo preview feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  # Form for post image
+  def form_image_select(post)
+    return image_tag post.image.url(:medium),
+                     id: 'image-preview',
+                     class: 'img-responsive' if post.image.exists?
+    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+  end
+
+
+  # Alert flash types
   def alert_for(flash_type)
     case flash_type
       when :success
